e9aadbf8d61ab2c9bae633415c8a5b125569271f
Tejas Mayekar <tejasmayekar08@gmail.com>
2023-07-06 09:40:38 +0530

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import {sha1,sha256,sha384,sha512} from 'crypto-hash';
 export default function Home() {
   const [text,setTextInput]=useState("")
   var [output,setOutput]=useState("")
-  const [password ,setPassword]=useState("")
+  const [password ,setPassword]=useState()
 
   const sendHash = async()=>{
     let response = await fetch(`https://api.pwnedpasswords.com/range/${output.slice(0,5)}`);
